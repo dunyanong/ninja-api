2a069503d7a3f0fd83526c80e39886395e0beb90
Dun Yan <ong.dy2004@gmail.com>
2023-07-04 01:56:08 +0800

    #3

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -7,28 +7,48 @@ import { UpdateNinjaDto } from './dto/update-ninja.dto';
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
 
+   // POST /ninjas
   @Post()
   create(@Body() createNinjaDto: CreateNinjaDto) {
-    return this.ninjasService.create(createNinjaDto);
+    console.log("POST /ninjas");
+    // Call the service to create a ninja using the provided DTO
+    const createdNinja = this.ninjasService.create(createNinjaDto);
+    return createdNinja;
   }
 
+  // GET /ninjas
   @Get()
   findAll() {
-    return this.ninjasService.findAll();
+    console.log("GET /ninjas");
+    // Call the service to get all ninjas
+    const ninjas = this.ninjasService.findAll();
+    return ninjas;
   }
 
+  // GET /ninjas/:id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ninjasService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    console.log("GET /ninjas/:id");
+    // Call the service to get a specific ninja by ID
+    const ninja = this.ninjasService.findOne(id);
+    return ninja;
   }
 
+  // PATCH /ninjas/:id
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
-    return this.ninjasService.update(+id, updateNinjaDto);
+  update(@Param('id') id: number, @Body() updateNinjaDto: UpdateNinjaDto) {
+    console.log("PATCH /ninjas/:id");
+    // Call the service to update a specific ninja by ID using the provided DTO
+    const updatedNinja = this.ninjasService.update(id, updateNinjaDto);
+    return updatedNinja;
   }
 
+  // DELETE /ninjas/:id
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ninjasService.remove(+id);
+  remove(@Param('id') id: number) {
+    console.log("DELETE /ninjas/:id");
+    // Call the service to delete a specific ninja by ID
+    const deletedNinja = this.ninjasService.remove(id);
+    return deletedNinja;
   }
 }
