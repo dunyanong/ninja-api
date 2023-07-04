4909e90f8dee17816b1542e88fd159197511cda8
Dun Yan <ong.dy2004@gmail.com>
2023-07-04 18:25:58 +0800

    #4

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,54 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+// CONTROLLER = BIG BOSS (receive requests and decide what to do next)
+
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put } from '@nestjs/common';
 import { NinjasService } from './ninjas.service';
-import { CreateNinjaDto } from './dto/create-ninja.dto';
-import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { CreateNinjaDto } from './dto/create-ninja.dto'; 
 
 @Controller('ninjas')
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
 
-   // POST /ninjas
-  @Post()
-  create(@Body() createNinjaDto: CreateNinjaDto) {
-    console.log("POST /ninjas");
-    // Call the service to create a ninja using the provided DTO
-    const createdNinja = this.ninjasService.create(createNinjaDto);
-    return createdNinja;
-  }
-
-  // GET /ninjas
   @Get()
-  findAll() {
-    console.log("GET /ninjas");
-    // Call the service to get all ninjas
-    const ninjas = this.ninjasService.findAll();
-    return ninjas;
+  getAllNinjas(): CreateNinjaDto[] {
+    console.log(this.ninjasService.getAllNinjas())
+    return this.ninjasService.getAllNinjas();
   }
 
-  // GET /ninjas/:id
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    console.log("GET /ninjas/:id");
-    // Call the service to get a specific ninja by ID
-    const ninja = this.ninjasService.findOne(id);
-    return ninja;
+  getNinjaById(@Param("id") id: number): CreateNinjaDto {
+    console.log(this.ninjasService.getNinjaById(id))
+    return this.ninjasService.getNinjaById(id);
   }
 
-  // PATCH /ninjas/:id
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateNinjaDto: UpdateNinjaDto) {
-    console.log("PATCH /ninjas/:id");
-    // Call the service to update a specific ninja by ID using the provided DTO
-    const updatedNinja = this.ninjasService.update(id, updateNinjaDto);
-    return updatedNinja;
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto): CreateNinjaDto {
+    console.log(this.ninjasService.createNinja(createNinjaDto))
+    return this.ninjasService.createNinja(createNinjaDto);
   }
 
-  // DELETE /ninjas/:id
+  @Put(':id')
+  updateNinja(
+    @Param('id') id: number,
+    @Body() createNinjaDto: CreateNinjaDto,
+  ): CreateNinjaDto {
+    console.log(this.ninjasService.updateNinja(id, createNinjaDto))
+    return this.ninjasService.updateNinja(id, createNinjaDto);
+  }
+
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    console.log("DELETE /ninjas/:id");
-    // Call the service to delete a specific ninja by ID
-    const deletedNinja = this.ninjasService.remove(id);
-    return deletedNinja;
+  deleteNinja(@Param('id') id: number): CreateNinjaDto {
+    console.log(this.ninjasService.deleteNinja(id))
+    return this.ninjasService.deleteNinja(id);
   }
 }
